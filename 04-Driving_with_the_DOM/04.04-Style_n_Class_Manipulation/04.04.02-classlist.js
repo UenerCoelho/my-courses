e057d6f4469062161a8f302f482393a8ca3aecab
Uener Coelho <uenercoelho2@hotmail.com>
2021-06-11 23:44:55 -0400

    Change classlit

---
+++ 04-Driving_with_the_DOM/04.04-Style_n_Class_Manipulation/04.04.02-classlist.js
@@ -0,0 +1,14 @@
+// Alterando Estilos
+// classlist
+
+const element = document.querySelector('body')
+
+element.classList.add('active', 'green')
+
+console.log(element.classList)
+
+element.classList.remove('active')
+
+element.classList.toggle('active')
+
+element.classList.toggle('green')
