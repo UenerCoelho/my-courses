9db5e679d236def5fbe71bea9dffa41f957385bf
Uener Coelho <uenercoelho2@hotmail.com>
2021-06-14 19:43:07 -0400

    Fix issues

--- 04-Driving_with_the_DOM/04.05-Navegando_pelos_elementos/04.05.02-Navegando_pelos_elementos_filhos.js
+++ 04-Driving_with_the_DOM/04.05-Navegando_pelos_elementos/04.05.02-Navegando_pelos_elementos_filhos.js
@@ -1,25 +1,19 @@
 // Navegando pelos elementos
 // childNodes children
 
-const elementChide = document.querySelector('body')
+const element = document.querySelector('body')
+const elementFirst = document.querySelector('body')
+const elementLast = document.querySelector('body')
 
-console.log(elementChide.childNodes)
+console.log(element.childNodes)
 
-// O children é um HTMLCollection, confira pelo devtools do browser
-console.log(elementChide.children)
+// firstChild levará em consideração os espaços vazios, como quebra de linha
+console.log(elementFirst.firstChild)
 
-/* Para o firstChild firstElementChild, verifique se seu editor de código está com o código "editor.formatOnSave": true, se estiver true, ele vai pular uma linha no código e deixar um child text na linha vazia, por isso o first childe sempre vai ser text.
-exemplo de erro:
-neste sempre o first childe do body será text
-<body> 
-  <header></header>
-</body>
+// firstElementChild não levará em consideração os espaços vazios
+console.log(elementFirst.firstElementChild)
 
-neste sempre não, será o header
-<body><header></header><body>
-*/
-console.log(elementChide.firstElementChild)
-
-// lastChild lastElementChild
-
-console.log(elementChide.lastElementChild)
+// lastChild levará em consideração os espaços vazios, como quebra de linha
+console.log(elementLast.lastChild)
+// lastElementChild não levará em consideração os espaços vazios
+console.log(elementLast.lastElementChild)
