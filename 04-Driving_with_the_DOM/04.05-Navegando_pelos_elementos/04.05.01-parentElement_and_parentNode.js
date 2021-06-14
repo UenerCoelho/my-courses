c87c5852f92389b45059f3b6414ce08a26bbbe3c
Uener Coelho <uenercoelho2@hotmail.com>
2021-06-14 18:47:54 -0400

    parentElement n parentNode

---
+++ 04-Driving_with_the_DOM/04.05-Navegando_pelos_elementos/04.05.01-parentElement_and_parentNode.js
@@ -0,0 +1,11 @@
+// Navegando pelos elementos
+// parentElement parentNode
+
+const body = document.querySelector('body')
+const element = document.querySelector('h1')
+
+console.log(body.parentElement)
+console.log(body.parentNode)
+
+console.log(element.parentElement)
+console.log(element.parentNode)
