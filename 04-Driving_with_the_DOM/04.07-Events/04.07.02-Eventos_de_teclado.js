2e0b77c6604134dfc556e590231a86120f86b264
Uener Coelho <uenercoelho2@hotmail.com>
2021-06-17 19:39:54 -0400

    adding keyboard events

---
+++ 04-Driving_with_the_DOM/04.07-Events/04.07.02-Eventos_de_teclado.js
@@ -0,0 +1,11 @@
+// Keyboard Events
+
+const input = document.querySelector('input')
+
+// input.onkeydown = function () {
+//   console.log('print')
+// }
+
+input.onkeypress = function () {
+  console.log('print')
+}
