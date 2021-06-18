33728e9b13a4fc70a3e07ce104f54bf236fb3043
Uener Coelho <uenercoelho2@hotmail.com>
2021-06-17 21:05:41 -0400

    Event as Argument

---
+++ 04-Driving_with_the_DOM/04.07-Events/04.07.05-Argumento_event.js
@@ -0,0 +1,11 @@
+// Event Argument
+
+const input = document.querySelector('input')
+
+input.onkeydown = function (event) {
+  // console.log(event)
+  // console.log(event.key)
+  // console.log(event.currentTarget)
+  console.log(event.currentTarget.value)
+
+}
