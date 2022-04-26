f99a7396865ed4fec59465735711fa5de761e716
Uener Coelho <uenercoelho2@hotmail.com>
2022-04-26 16:58:29 -0400

    Error Aula 424

--- 24-ProjetoCadastroDeUsuario/react/crud/frontend/src/main/App.jsx
+++ 24-ProjetoCadastroDeUsuario/react/crud/frontend/src/main/App.jsx
@@ -3,19 +3,20 @@ import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 import React from 'react';
 
+import { BrowserRouter } from 'react-router-dom';
+
 import Logo from '../components/template/Logo';
 import Nav from '../components/template/Nav';
-import Main from '../components/template/Main';
+import Routes from './Routes';
 import Footer from '../components/template/Footer';
 
 export default props =>
-  <div className="app">
-    <Logo />
-    <Nav />
-    <Main icon="home" title="Início" subtitle="Segundo Projeto do capítulo de React.">
-      <div className="display-4">Bem Vindo!</div>
-      <hr />
-      <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
-    </Main>
-    <Footer />
-  </div>
+
+  <BrowserRouter>
+    <div className="app">
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
+    </div>
+  </BrowserRouter>
