84727330e6fd1daa8b35dab4f7c63453d8557cb7
Uener Coelho <uenercoelho2@hotmail.com>
2022-06-07 18:45:30 -0400

    Herança 01

--- 07-JavaScript-Objeto/120-FuncaoImportantesObjeto.js
+++ 07-JavaScript-Objeto/120-FuncaoImportantesObjeto.js
@@ -23,9 +23,9 @@ console.log(pessoa.dataNascimento);
 console.log(Object.keys(pessoa));
 
 // Object.assign (ECMAScript 2015)
-const dest = {a: 1};
-const o1 = {b: 2};
-const o2 = {c: 3, a: 4};
+const dest = { a: 1 };
+const o1 = { b: 2 };
+const o2 = { c: 3, a: 4 };
 const obj = Object.assign(dest, o1, o2);
 
 Object.freeze(obj);
