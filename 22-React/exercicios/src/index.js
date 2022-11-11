ae1c25181bf81de864f3de93e7d0f5b26d518052
Uener Coelho <uenercoelho2@hotmail.com>
2022-11-11 18:52:25 -0400

    Componente Cde Classe React 01

--- 22-React/exercicios/src/index.js
+++ 22-React/exercicios/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Multi, { BoaNoite } from './componentes/Multiplos'
+import Saudacao from './componentes/Saudacao'
 
+// import Multi, { BoaNoite } from './componentes/Multiplos'
 // import Primeiro from './componentes/Primeiro'
 // import BomDia from './componentes/BomDia'
 // ReactDOM.render(<Primeiro />, document.getElementById('root'))
@@ -10,7 +11,6 @@ import Multi, { BoaNoite } from './componentes/Multiplos'
 
 ReactDOM.render(
   <div>
-    <Multi.BoaTarde nome='Ana' />
-    <BoaNoite nome='Bia' />
+    <Saudacao tipo="Bom dia" nome="João" />
   </div>
   , document.getElementById('root'))
