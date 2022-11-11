fa4424519eec9506135f0e7bbbd063703efef53f
Uener Coelho <uenercoelho2@hotmail.com>
2022-11-10 22:15:10 -0400

    Multi Componentes no React

--- 22-React/exercicios/src/index.js
+++ 22-React/exercicios/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Multi, { BoaNoite } from './componentes/Multiplos'
+
 // import Primeiro from './componentes/Primeiro'
-
-import BomDia from './componentes/BomDia'
-
+// import BomDia from './componentes/BomDia'
 // ReactDOM.render(<Primeiro />, document.getElementById('root'))
+// ReactDOM.render(<BomDia nome='Catia'/>, document.getElementById('root'))
 
-ReactDOM.render(<BomDia nome='Catia'/>, document.getElementById('root'))
+ReactDOM.render(
+  <div>
+    <Multi.BoaTarde nome='Ana' />
+    <BoaNoite nome='Bia' />
+  </div>
+  , document.getElementById('root'))
