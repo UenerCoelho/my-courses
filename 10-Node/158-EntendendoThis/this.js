aa860af21bda484e24d4b5c91b0e89545ffc3d21
Uener Coelho <uenercoelho2@hotmail.com>
2023-03-05 08:16:58 -0400

    Aula 158

--- 10-Node/158-EntendendoThis/this.js
+++ 10-Node/158-EntendendoThis/this.js
@@ -1,16 +1,33 @@
-console.log('"this" é estritamente igual a global?: ', this === global);
-console.log('"this" é estritamente igual a global?: ', this === module);
+console.log("#############=- OUT OF FUNCTION -=###############")
 
-console.log("############################################")
+console.log('"this" é === a global?: ', this === global);
+console.log('"this" é === a global?: ', this === module);
 
-console.log('"this" é estritamente igual a module.exports?: ', this === module.exports);
-console.log('"this" é estritamente igual a exports?: ', this === exports);
 
-console.log("############################################")
+console.log('"this" é === a module.exports?: ', this === module.exports);
+console.log('"this" é === a exports?: ', this === exports);
+
+console.log("#############=- INSIDE FUNCTION -=###############")
+
 
 function ThisOnFunction () {
-  console.log('this dentro de uma função... ');
-  console.log('"this" é estritamente igual a exports?: ', this === exports);
+  console.log('"this" DENTRO da FUNÇÃO é === a exports?:=> ', this === exports);
+  console.log('"this" DENTRO da FUNÇÃO é === a module.exports?:=> ', this === module.exports);
+  console.log('"this" DENTRO da FUNÇÃO é === a global?:=> ', this === global);
+  
 }
 
 ThisOnFunction();
+
+console.log("#############=- ARROW FUNCTION -=###############")
+/*  Porém, quando falamos de ARROW FUNCTION "() => {}", a coisa muda um pouco.
+Isso porque dentro de uma arrow function, o this não vai apontar mais para o objeto global, e sim para o module.export / exports. */
+
+const thisArrowFunction = () => {
+  console.log('"this" DENTRO da ARROW FUNÇÃO é === a exports?:=> ', this === exports);
+  console.log('"this" DENTRO da ARROW FUNÇÃO é === a module.exports?:=> ', this === module.exports);
+  console.log('"this" DENTRO da ARROW FUNÇÃO é === a global?:=> ', this === global);
+
+}
+
+thisArrowFunction();
