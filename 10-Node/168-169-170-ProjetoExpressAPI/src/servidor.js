64899dbcfbe4927a76366e0b3cf366c683ecee6d
Uener Coelho <uenercoelho2@hotmail.com>
2023-03-09 23:05:23 -0400

    Aula 169

--- 10-Node/168-169-170-ProjetoExpressAPI/src/servidor.js
+++ 10-Node/168-169-170-ProjetoExpressAPI/src/servidor.js
@@ -2,14 +2,22 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const dataBase = require('./db')
 
 app.get('/produtos', (req, res, next) => {
-  console.log('Middleware 1...')
-  next()
+  res.send(dataBase.getProdutos())
 })
 
-app.get('/produtos', (req, res, next) => {
-  res.send({ nome: 'Notebook', preço: 123.45 })
+app.get('/produtos/:id', (req, res, next) => {
+  res.send(dataBase.getProdutos(req.params.id))
+})
+
+app.post('/produto', (req, res, next) => {
+  const produto = dataBase.salvarProduto({
+    nome: req.body.name,
+    preco: req.body.preco
+  })
+  res.send(produto)
 })
 
 app.listen(porta, () => {
