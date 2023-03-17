53c110669fae375e4f2ccd43d42ac1a9719a0152
Uener Coelho <uenercoelho2@hotmail.com>
2023-03-16 22:53:43 -0400

    Function Anatomy 03

---
+++ 06.1-YTessentialJSfunctionalCouse2h/02-function-arrow.js
@@ -0,0 +1,19 @@
+// Arrow Function is always anonymous
+const arrowFn1 = (n) => {
+  return n +1
+}
+
+const arrowFn2 = n => {
+  return n + 1
+}
+
+// No caso abaixo, por não haver o par de chaves {} o 'return' está implícito, desde que fique na mesma linha
+const arrowFn3 = n => n + 1
+
+// No caso abixo, por não haver chaves {} e por haver 2 parâmetros, eles devem ficar entre parenteses, e como acima o 'return' está implícito
+const arrowFn31 = (a, b) => a + b
+
+console.log(arrowFn1(1));
+console.log(arrowFn2(4));
+console.log(arrowFn3(2));
+console.log(arrowFn31(2, 10));
