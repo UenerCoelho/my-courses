61ec1f2ab43b7e4f2cf8308b77843e1c1fb8af0d
Uener Coelho <uenercoelho2@hotmail.com>
2023-03-20 19:03:43 -0400

    Function Anatomy 04

--- 06.1-YTessentialJSfunctionalCouse2h/03-function-arrow.js
+++ 06.1-YTessentialJSfunctionalCouse2h/03-function-arrow.js
@@ -10,7 +10,7 @@ const arrowFn2 = n => {
 // No caso abaixo, por não haver o par de chaves {} o 'return' está implícito, desde que fique na mesma linha
 const arrowFn3 = n => n + 1
 
-// No caso abixo, por não haver chaves {} e por haver 2 parâmetros, eles devem ficar entre parenteses, e como acima o 'return' está implícito
+// No caso abaixo, por não haver chaves {} e por haver 2 parâmetros, eles devem ficar entre parenteses, e como acima o 'return' está implícito
 const arrowFn31 = (a, b) => a + b
 
 console.log(arrowFn1(1));
