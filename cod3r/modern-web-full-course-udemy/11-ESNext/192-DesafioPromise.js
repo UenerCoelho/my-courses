980c8a9bb628c6fba05b574885275f7d84174e74
Uener Coelho <uenercoelho2@hotmail.com>
2023-07-15 21:03:02 -0400

    Cod3r - aula 192

---
+++ cod3r/modern-web-full-course-udemy/11-ESNext/192-DesafioPromise.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+
+function readFile(contentPath) {
+  return new Promise(resolve => {
+    fs.readFile(contentPath, function (_, content) {
+      resolve(content.toString())
+    })
+    console.log('Depois de ler')
+  })
+}
+
+const contentPath = path.join(__dirname, '192-DesafioPromise.txt')
+
+readFile(contentPath)
+  .then(content => content.split('\n'))
+  // .then(linhas => console.log(linhas[1]))
+  // .then(linhas => console.log(linhas.length))
+  .then(linhas => linhas.join(','))
+  .then(content => `O valor final é: ${content}`)
+  .then(console.log)
