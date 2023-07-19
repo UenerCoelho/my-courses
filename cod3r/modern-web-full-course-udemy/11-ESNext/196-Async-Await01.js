ca54d44026716d30ebd31f5a00b5666ad460867b
Uener Coelho <uenercoelho2@hotmail.com>
2023-07-18 23:47:21 -0400

    Cod3r - aula 196

---
+++ cod3r/modern-web-full-course-udemy/11-ESNext/196-Async-Await01.js
@@ -0,0 +1,12 @@
+function waitFor(time = 2000) {
+  return new Promise(function (resolve) {
+    setTimeout(() => resolve(), time)
+  })
+}
+
+waitFor(2000)
+  .then(() => console.log('Executando promise 01...'))
+  .then(waitFor)
+  .then(() => console.log('Executando promise 02...'))
+  .then(waitFor)
+  .then(() => console.log('Executando promise 03...'))
