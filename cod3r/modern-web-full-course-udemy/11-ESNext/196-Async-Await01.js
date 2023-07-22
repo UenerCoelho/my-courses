e05ddbd565e410e3f422fdb1acd2872472c57f16
Uener Coelho <uenercoelho2@hotmail.com>
2023-07-21 22:02:54 -0400

    Cod3r - aula 196

--- cod3r/modern-web-full-course-udemy/11-ESNext/196-Async-Await01.js
+++ cod3r/modern-web-full-course-udemy/11-ESNext/196-Async-Await01.js
@@ -4,18 +4,40 @@ function waitFor(time = 2000) {
   })
 }
 
-waitFor(2000)
-  .then(() => console.log('Executando promise 01...'))
-  .then(waitFor)
-  .then(() => console.log('Executando promise 02...'))
-  .then(waitFor)
-  .then(() => console.log('Executando promise 03...'))
+// waitFor(2000)
+//   .then(() => console.log('Executando promise 01...'))
+//   .then(waitFor)
+//   .then(() => console.log('Executando promise 02...'))
+//   .then(waitFor)
+//   .then(() => console.log('Executando promise 03...'))
+
+function returnValue() {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(10), 5000)
+  })
+}
+
+async function returnFast() {
+  return 20
+}
 
 async function execute() {
-  waitFor(1)
-  console.log('async/Await 1...')
-  waitFor(2)
-  console.log('async/Await 1...')
-  waitFor(3)
-  console.log('async/Await 1...')
+  let value = await returnFast()
+  // let value = await returnValue()
+
+  await waitFor(1500)
+  console.log(`async/Await ${value}...`)
+  await waitFor(1500)
+  console.log(`async/Await ${value + 1}...`)
+  await waitFor(1500)
+  console.log(`async/Await ${value + 2}...`)
+
+  return value + 3
 }
+
+async function trueExecuting() {
+  const value = await execute()
+  console.log(value)
+}
+
+trueExecuting()
