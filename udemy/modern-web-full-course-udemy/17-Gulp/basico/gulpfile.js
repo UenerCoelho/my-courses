7405851b8d67a693fb7e37bec093056de104471b
Uener Coelho <uenercoelho2@hotmail.com>
2023-08-22 21:43:38 -0400

    cod3r aula 319

--- udemy/modern-web-full-course-udemy/17-Gulp/basico/gulpfile.js
+++ udemy/modern-web-full-course-udemy/17-Gulp/basico/gulpfile.js
@@ -1,11 +1,29 @@
 const gulp = require('gulp')
-// const { series } = require('gulp') // Pode ser feito deste jeito
-const series = gulp.series // Também pode ser feito deste jeito
+const { series, parallel } = require('gulp') // Pode ser feito deste jeito
+// const series = gulp.series // Também pode ser feito deste jeito
 
-function copiar(cb) {
-  // cb = callback
-  console.log('Tarefa de copiar')
+const antes1 = cb => {
+  console.log('Tarefa Antes 1!')
   return cb()
 }
 
-module.exports.default = series(copiar)
+const antes2 = cb => {
+  console.log('Tarefa Antes 2!')
+  return cb()
+}
+
+function copiar(cb) {
+  // cb = callback
+  gulp
+    // .src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    .src('pastaA/**/*.txt')
+    .pipe(gulp.dest('pastaC'))
+  return cb()
+}
+
+const fim = cb => {
+  console.log('Tarefa Fim!')
+  return cb()
+}
+
+module.exports.default = series(parallel(antes1, antes2), copiar, fim)
