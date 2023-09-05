a7822513a3cac6985fda648b0eccb7fd5906d1dd
Uener Coelho <uenercoelho2@hotmail.com>
2023-09-04 22:21:10 -0400

    game development aula 13

---
+++ udemy/game-development/Dawn Forest/_12_strings.gd
@@ -0,0 +1,8 @@
+extends Node2D
+class_name _12_strings
+
+var hello = "Hello"
+var player_name = "David!"
+
+func _ready():
+	print(hello + " " + player_name)
---
+++ udemy/game-development/Dawn Forest/_13_float.gd
@@ -0,0 +1,8 @@
+extends Node2D
+class_name _13_float
+
+var a_float = 1.33
+var b_float = 1.52
+
+func _ready():
+	print(a_float + b_float)
