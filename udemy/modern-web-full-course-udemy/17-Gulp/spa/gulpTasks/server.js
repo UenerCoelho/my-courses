aaf24ea34ccab06108254df6bffa45f097a05846
Uener Coelho <uenercoelho2@hotmail.com>
2023-09-08 20:38:18 -0400

    cod3r aula 329

---
+++ udemy/modern-web-full-course-udemy/17-Gulp/spa/gulpTasks/server.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp')
+
+function monitorarArquivos(cb) {
+  return cb()
+}
+
+function server(cb) {
+  return cb()
+}
+
+module.exports = {
+  monitorarArquivos,
+  server
+}
