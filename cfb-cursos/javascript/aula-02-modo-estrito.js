3b7b4968ab4d076d35ad9555a8805af1596bca90
Uener Coelho <uenercoelho2@hotmail.com>
2023-09-13 18:49:52 -0400

    cfb-cursos-js course - aula 02

--- cfb-cursos/javascript/aula-02-modo-estrito.js
+++ cfb-cursos/javascript/aula-02-modo-estrito.js
@@ -1,2 +1,5 @@
+'use strict' // Modo Estrito, serve para deixar o código de uma forma mais limpo, como criar uma variável sem declará-la (usando "let", "const" ou até mesmo "var")
 let nome = 'Uener'
+console.log('Eu sou ' + nome)
 console.log(nome)
+console.log(nome + ', Este sou Eu')
