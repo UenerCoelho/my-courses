905a7c21008241f870c88f48f89a9441f6fd7063
Uener Coelho <uenercoelho2@hotmail.com>
2023-09-15 18:37:35 -0400

    cfb-cursos-js course - aula 05

---
+++ cfb-cursos/javascript/aula-05-operadores-relacionais.js
@@ -0,0 +1,17 @@
+/* Operadores Relacionais
+    > Maior
+    >= Maior ou Igual
+    < Menor
+    <= Menor ou Igual
+    == Igual (dois sinais de =)
+    != Diferente (1 sinal de ! e outro de =)
+*/
+
+let num1 = 1
+let num2 = 2
+console.log(`1 é maior que 2? ${num1 > num2}`)
+console.log(`1 é menor que 2? ${num1 < num2}`)
+console.log(`1 é maior ou igual que 2? ${num1 >= num2}`)
+console.log(`1 é menor ou igual que 2? ${num1 <= num2}`)
+console.log(`1 é igual que 2? ${num1 == num2}`)
+console.log(`1 é diferente que 2? ${num1 != num2}`)
---
+++ cfb-cursos/javascript/aula-07-operadores-logicos.js
@@ -0,0 +1,7 @@
+/* Operadores Lógicos
+    ! Negação
+    === Estritamente Igual (três sinais de =)
+    !== Estritamente Diferente (um sinal de ! e dois =)
+    || ou
+    && and (e)
+*/
