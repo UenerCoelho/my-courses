679919117f3e66fa0f2df7f812ac5a0ded6a6cbe
Uener Coelho <uenercoelho2@hotmail.com>
2023-09-28 21:27:47 -0400

    cfb-cursos-js course - aula 11

---
+++ cfb-cursos/javascript/aula-011-operador-typeof.js
@@ -0,0 +1,9 @@
+let number = 10
+let string = '10'
+let boolean = number === string
+let objeto = { name: 'Uener' }
+
+console.log(`Valor: ${number}  - do Tipo: ${typeof number}`)
+console.log(`Valor: ${string}  - do Tipo: ${typeof string}`)
+console.log(`Valor: ${boolean}  - do Tipo: ${typeof boolean}`)
+console.log(`Valor: ${objeto}  - do Tipo: ${typeof objeto}`)
