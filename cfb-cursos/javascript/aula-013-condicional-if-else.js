d11bd68071880b3c14c8bc0d85b9bc557a2c6b38
Uener Coelho <uenercoelho2@hotmail.com>
2023-10-03 20:55:54 -0400

    cfb-cursos-js course - aula 13

---
+++ cfb-cursos/javascript/aula-013-condicional-if-else.js
@@ -0,0 +1,16 @@
+let num = 99
+
+if (num > 10) {
+  console.log('numeral maior que 10')
+  // Aninhamento de IF
+  if (num > 50) {
+    console.log('numeral maior que 50')
+    if (num < 100) {
+      console.log('numeral menor que 100')
+    }
+  }
+} else if (num < 10) {
+  console.log('numeral menor que 10')
+} else {
+  console.log('numeral igual a 10')
+}
