83c184fc3401195ec01ae9b62cdea5edcfb07f50
Uener Coelho <uenercoelho2@hotmail.com>
2023-10-30 20:52:06 -0400

    cfb-cursos-js course - aula 14

---
+++ cfb-cursos/javascript/aula-014-switch-case.js
@@ -0,0 +1,21 @@
+let position = 5
+
+switch (position) {
+  case 1:
+    console.log('Primeiro Lugar')
+    break
+  case 2:
+    console.log('Segundo Lugar')
+    break
+  case 3:
+    console.log('Terceiro Lugar')
+    break
+  case 4:
+  case 5:
+  case 6:
+    console.log('Premio de consolação')
+    break
+  default:
+    console.log('Não subiu ao pódio')
+    break
+}
