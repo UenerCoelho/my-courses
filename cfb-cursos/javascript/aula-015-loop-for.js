abbd8f738843ce12075dd8257fca3a6b6b4c5d56
Uener Coelho <uenercoelho2@hotmail.com>
2023-11-02 19:15:06 -0400

    cfb-cursos-js course - aula 15

---
+++ cfb-cursos/javascript/aula-015-loop-for.js
@@ -0,0 +1,11 @@
+// For é um tipo de loop Definido, quando se sabe a quantidade de itens que o loop fará a iteração
+
+for (let i = 0; i < 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i + ' é par')
+  } else {
+    console.log(i + ' é impar')
+  }
+}
+
+console.log('fora do For')
