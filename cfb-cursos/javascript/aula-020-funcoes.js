b1eca6202f0ccab56b19bef92fa04b00adac81cc
Uener Coelho <uenercoelho2@hotmail.com>
2023-11-15 19:42:43 -0400

    cfb-cursos-js course - aula 20

---
+++ cfb-cursos/javascript/aula-020-funcoes.js
@@ -0,0 +1,20 @@
+function functionName() {
+  let num1 = 2
+  let num2 = 10
+  let soma = num1 + num2
+  console.log(soma)
+  let d1 = document.getElementById('d1')
+  let d2 = document.getElementById('d2')
+  let d3 = document.getElementById('d3')
+  let d4 = document.getElementById('d4')
+  d1.innerHTML = 'Coelho, Uener'
+  d2.innerHTML = 'Coelho, Catia'
+  d3.innerHTML = 'Coelho, David'
+  d4.innerHTML = 'Coelho, Leonardo'
+}
+
+// functionName()
+
+for (let i = 0; i <= 10; i++) {
+  // functionName()
+}
