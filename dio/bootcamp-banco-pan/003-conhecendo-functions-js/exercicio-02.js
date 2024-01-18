0fe5b5455d97334a4719e9c38035783429b62443
Uener Coelho <uenercoelho2@hotmail.com>
2024-01-17 21:22:13 -0400

    DIO Banco Pan Bootcamp - Javascript intro

---
+++ dio/bootcamp-banco-pan/003-conhecendo-functions-js/exercicio-02.js
@@ -0,0 +1,14 @@
+;(() => {
+  const name = 'Uener'
+  const age = 4
+
+  if (age < 18) {
+    console.log(
+      `${name}, você é menor de idade, pois sua idade é: ${age}, que é menor que 18!`
+    )
+  } else {
+    console.log(
+      `${name}, você é maior de idade, pois sua idade é: ${age}, que é maior que 18!`
+    )
+  }
+})()
