c5a9b1f124a1e4ef7f1248dd74f1dd403693a8e5
Uener Coelho <uenercoelho2@hotmail.com>
2024-01-29 22:14:21 -0400

    DIO Banco Pan Bootcamp - Javascript intro

---
+++ dio/bootcamp-banco-pan/006-importacao-exportacao-com-js/main.js
@@ -0,0 +1,8 @@
+// const functions = require('./auxiliary-functions')
+
+// console.log(functions)
+
+// Object destructuring
+const { gets, print } = require('./auxiliary-functions')
+
+print(gets())
