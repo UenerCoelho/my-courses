a2467f7c77ab822a6d9c9b0c6cff7d7a9a07dfdb
Uener Coelho <uenercoelho2@hotmail.com>
2024-01-29 22:35:52 -0400

    DIO Banco Pan Bootcamp - Javascript intro

---
+++ dio/bootcamp-banco-pan/006-importacao-exportacao-com-js/exercises-functions-01.js
@@ -0,0 +1,14 @@
+const entryValues = [5, 50, 10, 98, 23]
+let i = 0
+
+function gets() {
+  const value = entryValues[i]
+  i++
+  return value
+}
+
+function print(text) {
+  console.log(text)
+}
+
+module.exports = { gets: gets, print: print }
---
+++ dio/bootcamp-banco-pan/006-importacao-exportacao-com-js/exxercises-main-01.js
@@ -0,0 +1,31 @@
+/*  Numa sala contem 5 alunos e para cada aluno foi sorteado um número de 1 - 100.
+    Faça um programa que receba os 5 números sorteados para os alunos e mostre o maior número sorteado.
+
+    Dados de entrada::
+    5
+    50
+    10
+    98
+    23
+
+    Saída:
+    98
+*/
+const { gets, print } = require('./exercises-functions-01')
+
+const sortedNumbers = []
+for (let i = 0; i < 5; i++) {
+  const sortedNumber = gets()
+  sortedNumbers.push(sortedNumber)
+}
+
+let highestValue = 0
+
+for (let i = 0; i < sortedNumbers.length; i++) {
+  const sortedNumber = sortedNumbers[i]
+  if (sortedNumber > highestValue) {
+    highestValue = sortedNumber
+  }
+}
+
+print(highestValue)
