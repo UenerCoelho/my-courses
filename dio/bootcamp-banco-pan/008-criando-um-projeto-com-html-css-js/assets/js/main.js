a00fd2c8899b66d8d91b506d2d6a09462be2c72f
Uener Coelho <uenercoelho2@hotmail.com>
2024-02-16 22:14:02 -0400

    DIO Banco Pan Bootcamp - Javascript intro

--- dio/bootcamp-banco-pan/008-criando-um-projeto-com-html-css-js/assets/js/main.js
+++ dio/bootcamp-banco-pan/008-criando-um-projeto-com-html-css-js/assets/js/main.js
@@ -2,7 +2,34 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url)
-  .then(response => response.json())
-  .then(jsonBody => console.log(jsonBody))
-  .catch(error => console.log(error))
+function convertPokemonTypesToLi(pokemonTypes) {
+  return pokemonTypes.map(
+    typeSlot => `<li class="type">${typeSlot.type.name}</li>`
+  )
+}
+
+function convertPokemonToHtml(pokemon) {
+  return `
+  <li class="pokemon">
+  <span class="number">#${pokemon.order}</span>
+  <span class="name">${pokemon.name}</span>
+
+  <div class="detail">
+    <ol class="types">
+      ${convertPokemonTypesToLi(pokemon.types).join('')}
+    </ol>
+    <img 
+      src="${pokemon.sprites.other.dream_world.front_default}" 
+      alt="${pokemon.name}" 
+    />
+  </div>
+</li>
+  `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+  const newHTML = pokemons.map(convertPokemonToHtml).join('')
+  pokemonList.innerHTML += newHTML
+})
