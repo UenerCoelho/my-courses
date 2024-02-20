e56283fbb80554c0a889e522ea03c84ce5e8b460
Uener Coelho <uenercoelho2@hotmail.com>
2024-02-20 19:25:58 -0400

    DIO Banco Pan Bootcamp - Javascript intro

--- dio/bootcamp-banco-pan/008-criando-um-projeto-com-html-css-js/assets/js/main.js
+++ dio/bootcamp-banco-pan/008-criando-um-projeto-com-html-css-js/assets/js/main.js
@@ -1,31 +1,47 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 6
+let offset = 0
+const maxRecord = 151
 
-function convertPokemonToHtml(pokemon) {
-  return `
-  <li class="pokemon ${pokemon.type}">
-  <span class="number">#${pokemon.number}</span>
-  <span class="name">${pokemon.name}</span>
-
-  <div class="detail">
-    <ol class="types">
-      ${pokemon.types
-        .map(type => `<li class="type ${type}">${type}</li>`)
-        .join('')}
-    </ol>
-    <img 
-      src="${pokemon.photo}" 
-      alt="${pokemon.name}" 
-    />
-  </div>
-</li>
-  `
+function loadPokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHTML = pokemons
+      .map(
+        pokemon => `
+            <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+          
+            <div class="detail">
+              <ol class="types">
+                ${pokemon.types
+                  .map(type => `<li class="type ${type}">${type}</li>`)
+                  .join('')}
+              </ol>
+              <img 
+                src="${pokemon.photo}" 
+                alt="${pokemon.name}" 
+              />
+            </div>
+          </li>
+        `
+      )
+      .join('')
+    pokemonList.innerHTML += newHTML
+  })
 }
 
-const pokemonList = document.getElementById('pokemonList')
+loadPokemonItens(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-  const newHTML = pokemons.map(convertPokemonToHtml).join('')
-  pokemonList.innerHTML += newHTML
+loadMoreButton.addEventListener('click', () => {
+  offset += limit
+  const quantityRecord = offset + limit
+  if (quantityRecord >= maxRecord) {
+    const newLimit = quantityRecord - offset
+    loadPokemonItens(offset, newLimit)
+    loadMoreButton.parentElement.removeChild(loadMoreButton)
+  } else {
+    loadPokemonItens(offset, limit)
+  }
 })
