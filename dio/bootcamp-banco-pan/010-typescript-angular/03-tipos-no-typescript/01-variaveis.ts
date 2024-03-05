8066eddda938a8a4ae0199d064ab243db2f3ca8d
Uener Coelho <uenercoelho2@hotmail.com>
2024-03-05 18:34:53 -0400

    DIO Banco Pan Bootcamp - Fundamentos Angular

---
+++ dio/bootcamp-banco-pan/010-typescript-angular/03-tipos-no-typescript/01-variaveis.ts
@@ -0,0 +1,13 @@
+// Tipos primitivos: boolean, number, string
+let ligado: boolean = false || true;
+let nome: string = 'Uener';
+let idade: number = 40;
+let altura: number = 1.84;
+
+// Tipos especiais: null, undefined
+let nulo: null = null
+let indefinido: undefined = undefined;
+
+// Tipos abrangentes: any, void
+let vazio: void
+let qualquerCoisa: any = 'Nome' || 1 || false || true
