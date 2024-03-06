e8b091b953e8e9900d9386c793433ed42039f24c
Uener Coelho <uenercoelho2@hotmail.com>
2024-03-05 23:24:53 -0400

    DIO Banco Pan Bootcamp - Fundamentos Angular

---
+++ dio/bootcamp-banco-pan/010-typescript-angular/03-tipos-no-typescript/05-tuplas.ts
@@ -0,0 +1,5 @@
+// Tupla é um vetor (array) de multi-type, deve ser apresentado da maneira em que se seta os tipos, como no exemplo abaixo.
+let boleto:[string, number, number]=["água conta", 199.90, 987654321]
+let boleto1:[boolean, string, number]=[true, "água conta", 199.90]
+let boleto2:[number, string, number]=[987654321, "água conta", 199.90]
+let boleto3:[string, number, boolean]=["água conta", 199.90, false]
