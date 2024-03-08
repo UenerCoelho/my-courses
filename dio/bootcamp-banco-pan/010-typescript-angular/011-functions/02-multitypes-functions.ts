9350d92b11a12563318962d0cb870aa7620bad13
Uener Coelho <uenercoelho2@hotmail.com>
2024-03-07 20:43:38 -0400

    DIO Banco Pan Bootcamp - Fundamentos Angular

---
+++ dio/bootcamp-banco-pan/010-typescript-angular/011-functions/02-multitypes-functions.ts
@@ -0,0 +1,9 @@
+// Explícita
+function callToPhoneExplicit(phone: number | string): number | string{{
+  return phone
+}}
+
+// Implícita
+function callToPhoneImplicit(phone: number | string){
+  return phone
+}
