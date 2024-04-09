7736da76b1264cbe4c551c346a3b752942aa29d0
Uener Coelho <uenercoelho2@hotmail.com>
2024-04-09 15:30:33 -0400

    DIO Banco Pan Bootcamp - Fundamentos Angular

--- dio/bootcamp-banco-pan/012-single-page-application/01-intro/02-diretivas/src/app/card/card.component.ts
+++ dio/bootcamp-banco-pan/012-single-page-application/01-intro/02-diretivas/src/app/card/card.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
