a3295cc32c3795ffbf3660f1e25c4a1aed0c993e
Uener Coelho <uenercoelho2@hotmail.com>
2024-04-15 19:09:55 -0400

    DIO Banco Pan Bootcamp - Fundamentos Angular

--- dio/bootcamp-banco-pan/012-single-page-application/01-intro/02-diretivas/src/app/comp-atributos/comp-atributos.component.ts
+++ dio/bootcamp-banco-pan/012-single-page-application/01-intro/02-diretivas/src/app/comp-atributos/comp-atributos.component.ts
@@ -11,6 +11,7 @@ export class CompAtributosComponent implements OnInit {
   corFonte:string = 'white'
   item:string = ''
   lista:string [] = []
+  isEnableBlock:boolean = false
 
   constructor() { }
   adicionarLista(){
