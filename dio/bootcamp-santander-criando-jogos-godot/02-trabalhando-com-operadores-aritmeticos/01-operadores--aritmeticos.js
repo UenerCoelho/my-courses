da85d360de4be4fd436ffa6069a03a32276c1bb0
Uener Coelho <uenercoelho2@hotmail.com>
2024-04-30 19:53:29 -0400

    Bootcamp Santander

---
+++ dio/bootcamp-santander-criando-jogos-godot/02-trabalhando-com-operadores-aritmeticos/01-operadores--aritmeticos.js
@@ -0,0 +1,42 @@
+let idade = 30
+console.log('Valor da minha variável ' + idade)
+
+idade = 30 + 6
+
+console.log('Operação de adição, 30 + 6 = ' + idade)
+
+idade = 30 / 6
+
+console.log('Operação de Divisão 30 / 6 = ' + idade)
+
+let primeiroNumero = 1023
+let segundoNumero = 23
+console.log(
+  'Operação de Subtração, 1023 - 23 = ' + (primeiroNumero - segundoNumero)
+)
+
+let productPrice = 100.99
+let taxValue = productPrice * 2
+console.log('Operação de Multiplicação, ' + productPrice + ' * 2 = ' + taxValue)
+
+let multiplier = 4
+let multiplying = 12
+
+let product = multiplier * multiplying
+console.log(
+  `Operação de Multiplicação, ${multiplier} * ${multiplying} = ${product}`
+)
+
+multiplier = 8
+product = multiplier * multiplying
+console.log(
+  `Operação de Multiplicação, ${multiplier} * ${multiplying} = ${product}`
+)
+
+let divider = 50
+let dividend = 2
+let result = divider / dividend
+console.log(`Operação de Divisão, ${divider} / ${dividend} = ${result}`)
+
+let calcModule = 10 % 3
+console.log('Operação de Modulo, 10 % 3 = ' + calcModule)
