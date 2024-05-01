0dc19cc8f302d2d31e80ed8da52035f4379aa8b0
Uener Coelho <uenercoelho2@hotmail.com>
2024-04-30 20:06:03 -0400

    Bootcamp Santander

---
+++ dio/bootcamp-santander-criando-jogos-godot/02-trabalhando-com-operadores-aritmeticos/03-operadores-atribuicao.js
@@ -0,0 +1,9 @@
+let price = 10
+console.log('Valor da variável price = ' + price)
+
+price += 5
+console.log('Atribuindo += 5 = ' + price)
+price += 5
+console.log('Atribuindo += 5 = ' + price)
+price -= 5
+console.log('Atribuindo -= 5 (menos 5) = ' + price)
