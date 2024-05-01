c073303fdc1f00350ee279ecc13954489472240c
Uener Coelho <uenercoelho2@hotmail.com>
2024-04-30 20:02:42 -0400

    Bootcamp Santander

---
+++ dio/bootcamp-santander-criando-jogos-godot/02-trabalhando-com-operadores-aritmeticos/02-incremento-decremento.js
@@ -0,0 +1,14 @@
+let counter = 1
+console.log('Valor da variável counter = ' + counter)
+counter++
+console.log('Incremento "+ 1" = ' + counter)
+counter++
+console.log('Incremento "+ 1" = ' + counter)
+counter++
+console.log('Incremento "+ 1" = ' + counter)
+counter--
+console.log('Decremento "- 1" = ' + counter)
+counter--
+console.log('Decremento "- 1" = ' + counter)
+counter--
+console.log('Decremento "- 1" = ' + counter)
