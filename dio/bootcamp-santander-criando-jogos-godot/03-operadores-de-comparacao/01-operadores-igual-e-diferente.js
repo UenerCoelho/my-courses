5877a923e46a2b681a4b73da928d297d65cf135c
Uener Coelho <uenercoelho2@hotmail.com>
2024-05-02 19:48:47 -0400

    Bootcamp Santander

---
+++ dio/bootcamp-santander-criando-jogos-godot/03-operadores-de-comparacao/01-operadores-igual-e-diferente.js
@@ -0,0 +1,18 @@
+let number = '1'
+console.log(number === '1')
+
+// =   é atribuição
+// ==  é para comparar o valor
+// === é para comparar o valor e o formato do conteúdo
+
+let marca = 'Apple'
+console.log(marca === 'Apple')
+console.log(marca !== 'Apple')
+console.log(marca !== 'Samsung')
+
+let cpfLocked = '123.456.789-00'
+let userCPF = '001.234.567-89'
+let blockedUser = cpfLocked === userCPF
+console.log('O usuário está barrado? ' + blockedUser)
+blockedUser = cpfLocked !== userCPF
+console.log('O usuário está barrado? ' + blockedUser)
