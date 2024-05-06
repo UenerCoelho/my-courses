f539c14e1d2e1d8b82f7676c53b9f4c7d90626b6
Uener Coelho <uenercoelho2@hotmail.com>
2024-05-06 19:33:54 -0400

    Bootcamp Santander

---
+++ dio/bootcamp-santander-criando-jogos-godot/05-estruturas-condicionais/01-condicional-if.js
@@ -0,0 +1,15 @@
+let onButton = true
+
+if (onButton == true) {
+  console.log('Comando Ligar')
+}
+
+let ovos = true
+let itensComprados
+
+if (ovos == true) {
+  itensComprados = 'Leite'
+  console.log('Além de ovos vc tem ' + itensComprados)
+} else {
+  console.log('Você não comprou os ovos')
+}
