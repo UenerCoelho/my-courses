e2bd025268c478951f8d1343110637c27826ac92
Uener Coelho <uenercoelho2@hotmail.com>
2024-05-06 21:39:05 -0400

    Bootcamp Santander

---
+++ dio/bootcamp-santander-criando-jogos-godot/05-estruturas-condicionais/03-else-if.js
@@ -0,0 +1,9 @@
+let hungerLevel = ''
+
+if (hungerLevel === 1) {
+  console.log('Você está com muita fome, então comeremos churrasco.')
+} else if (hungerLevel === 2) {
+  console.log('Você está com pouca fome, então comeremos pipoca.')
+} else {
+  console.log('Você está sem fome, então comeremos nada.')
+}
