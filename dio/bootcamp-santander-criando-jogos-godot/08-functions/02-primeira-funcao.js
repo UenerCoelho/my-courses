cde06f0b4716f603adf8da16dfe60df7ba813a4a
Uener Coelho <uenercoelho2@hotmail.com>
2024-05-15 15:21:20 -0400

    Bootcamp Santander

---
+++ dio/bootcamp-santander-criando-jogos-godot/08-functions/02-primeira-funcao.js
@@ -0,0 +1,11 @@
+torrar()
+
+function torrar() {
+  console.log('Torrando o Pão!')
+  ColocarPao()
+}
+
+function ColocarPao() {
+  console.log('Preparando para colocar o pão')
+  console.log('Finalizado!')
+}
