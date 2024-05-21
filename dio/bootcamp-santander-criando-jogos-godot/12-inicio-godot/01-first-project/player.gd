2e5b251d72a543ead7bec7010fd157f971e15f2d
Uener Coelho <uenercoelho2@hotmail.com>
2024-05-21 10:57:56 -0400

    Bootcamp Santander

--- dio/bootcamp-santander-criando-jogos-godot/12-inicio-godot/01-first-project/player.gd
+++ dio/bootcamp-santander-criando-jogos-godot/12-inicio-godot/01-first-project/player.gd
@@ -1,5 +1,6 @@
 extends Sprite2D
 
+@export var speed = 10.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,5 +9,8 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta: float) -> void:
+	var input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	position += input * speed * delta
 	pass
