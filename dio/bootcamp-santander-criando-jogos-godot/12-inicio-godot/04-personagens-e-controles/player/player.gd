b0e95918b0c55f0ccbaba0932b12f5d0c05a3f67
Uener Coelho <uenercoelho2@hotmail.com>
2024-05-31 20:07:55 -0400

    Bootcamp Santander

--- dio/bootcamp-santander-criando-jogos-godot/12-inicio-godot/04-personagens-e-controles/player/player.gd
+++ dio/bootcamp-santander-criando-jogos-godot/12-inicio-godot/04-personagens-e-controles/player/player.gd
@@ -1,15 +1,50 @@
 extends CharacterBody2D
 
+@export var speed: float = 3
 
+@onready var sprite: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var is_running: bool = false
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
+func _physics_process(delta: float) -> void:
+	# Obter o Input_vector
+	var input_vector = Input.get_vector("move_left", "move_rigth", "move_up", "move_down")
+	
+	# Apagar DeadZone do input vector
+	var deadzone = 0.15
+	if abs(input_vector.x) < 0.15:
+		input_vector.x = 0.0
+	if abs(input_vector.y) < 0.15:
+		input_vector.y = 0.0
+	
+	
+	# Modificar a Velocidade
+	var target_velocity = input_vector * speed * 100.00
+	velocity = lerp(velocity, target_velocity, 0.05)
+	move_and_slide()
+	
+	# Atualizar o is_running
+	var was_running = is_running
+	is_running = not input_vector.is_zero_approx()
+	
+	# Tocar Animação
+	if was_running != is_running:
 		if is_running:
-			animation_player.play("idle")
-			is_running = false
-		else:
 			animation_player.play("run")
-			is_running = true
+		else:
+			animation_player.play("idle")
+		
+	# Girar Sprite2D
+	if input_vector.x > 0:
+		# Desmarcar Flip_H do Sprite2D
+		sprite.flip_h = false
+	elif input_vector.x < 0:
+		# Marcar Flip_H do Sprite2D
+		sprite.flip_h = true
+	#elif input_vector.y > 0:
+		## Desmarcar Flip_V do Sprite
+		#sprite.flip_v = false
+	#elif input_vector.y < 0:
+		## Marcar Flip_V do Sprite
+		#sprite.flip_v = true
