86220ff782e2da71b4c4a4b9ff66a6057d7efa77
Uener Coelho <uenercoelho2@hotmail.com>
2024-06-18 19:01:40 -0400

    Bootcamp Santander

--- dio/bootcamp-santander-criando-jogos-godot/12-inicio-godot/05-as-porcas-e-parafusos-de-um-jogo/enemies/enemy.gd
+++ dio/bootcamp-santander-criando-jogos-godot/12-inicio-godot/05-as-porcas-e-parafusos-de-um-jogo/enemies/enemy.gd
@@ -2,7 +2,26 @@ class_name Enemy
 extends Node2D
 
 @export var health: int = 10
+@export var death_prefab: PackedScene
 
 func damage(amount: int) -> void:
 	health -= amount
 	print("Dano Recebid de ", amount, ". Vida restante de: ", health,"!")
+	
+	# Piscar Node
+	modulate = Color.DARK_RED
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUINT).tween_property(self, "modulate", Color.WHITE, 0.3)
+	
+	# Processar Morte
+	if health <= 0:
+		die()
+		
+
+func die() -> void:
+	if death_prefab:
+		var death_object = death_prefab.instantiate()
+		death_object.position = position
+		get_parent().add_child(death_object)
+		
+	queue_free()
