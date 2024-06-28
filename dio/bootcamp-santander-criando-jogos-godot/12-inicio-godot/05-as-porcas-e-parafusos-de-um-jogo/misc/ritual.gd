00c00b95ec03260986c2dfde5aa6e843354c2efc
Uener Coelho <uenercoelho2@hotmail.com>
2024-06-27 20:53:08 -0400

    Bootcamp Santander

--- dio/bootcamp-santander-criando-jogos-godot/12-inicio-godot/05-as-porcas-e-parafusos-de-um-jogo/misc/ritual.gd
+++ dio/bootcamp-santander-criando-jogos-godot/12-inicio-godot/05-as-porcas-e-parafusos-de-um-jogo/misc/ritual.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
+@export var damage_amount:int = 1
 
+# Area2D Collisionshape2D
+@onready var area2d: Area2D = $Area2D
 
 
 func deal_damage():
-	print("Ritual Damage")
-	pass
+	# get_overlaping_bodies
+	var bodies = area2d.get_overlapping_bodies()
+	# is_in_group("ememies")
+	for body in bodies:
+		if body.is_in_group("enemies"):
+			var enemy: Enemy = body
+			# enemy.damage
+			enemy.damage(damage_amount)
--- dio/bootcamp-santander-criando-jogos-godot/12-inicio-godot/05-as-porcas-e-parafusos-de-um-jogo/player/player.gd
+++ dio/bootcamp-santander-criando-jogos-godot/12-inicio-godot/05-as-porcas-e-parafusos-de-um-jogo/player/player.gd
@@ -1,11 +1,21 @@
 class_name Player
 extends CharacterBody2D
 
+@export_category("Movement")
 @export var speed: float = 3
+
+@export_category("Weapons")
 @export var sword_damage: int = 1
+
+@export_category("Life")
 @export var health: int = 100
 @export var max_health: int = 100
 @export var death_prefab: PackedScene
+
+@export_category("Magic")
+@export var ritual_damage: int = 1
+@export var ritual_interval: float = 5
+@export var ritual_scene: PackedScene
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -18,6 +28,7 @@ var was_running: bool = false
 var is_attacking: bool = false
 var attack_cooldown: float = 0.0
 var hitbox_cooldown: float = 0.0
+var ritual_cooldown: float = 0.0
 
 func _process(delta: float) -> void:
 	
@@ -41,6 +52,17 @@ func _process(delta: float) -> void:
 		rotate_sprite()
 	
 	update_hitbox_detection(delta)
+	
+	update_ritual(delta)
+
+func update_ritual(delta: float) -> void:
+	ritual_cooldown -= delta
+	if ritual_cooldown > 0: return
+	ritual_cooldown = ritual_interval
+	
+	var ritual = ritual_scene.instantiate()
+	ritual.damage_amount = ritual_damage
+	add_child(ritual)
 
 func read_input() -> void:
 	# Obter o Input_vector
