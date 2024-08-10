6ef6d24eba7841622a0c55c90d4b836995392f2c
Uener Coelho <uenercoelho2@hotmail.com>
2024-08-09 22:43:25 -0400

    DIO Banco Pan Bootcamp - Fundamentos Angular

--- dio/bootcamp-banco-pan/016-trabalhando-com-services-angular/01-angular-services/src/app/components/card/card.component.ts
+++ dio/bootcamp-banco-pan/016-trabalhando-com-services-angular/01-angular-services/src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -9,9 +10,17 @@ export class CardComponent implements OnInit {
   name:string = 'Bulbassaur'
   attributesTypes:string[] = ['FIRE', 'ROCK']
 
-  constructor() { }
+  constructor(
+    private service:PokemonService
+  ) { }
 
   ngOnInit(): void {
+    this.service.getPokemon('charizard').subscribe(
+      {
+        next: (res) => console.log(res),
+        error: (err) => console.log(err)
+      }
+    )
   }
 
 }
