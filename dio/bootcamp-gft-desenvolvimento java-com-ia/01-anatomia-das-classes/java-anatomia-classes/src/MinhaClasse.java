35ef783354b18e099c4e167789e2df42acb78be4
Uener Coelho <uenercoelho2@hotmail.com>
2024-08-21 21:34:09 -0400

    DIO Bootcamp GFT - Desenvolvimento Java com AI

--- dio/bootcamp-gft-desenvolvimento java-com-ia/01-anatomia-das-classes/java-anatomia-classes/src/MinhaClasse.java
+++ dio/bootcamp-gft-desenvolvimento java-com-ia/01-anatomia-das-classes/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,14 @@
 public class MinhaClasse {
   public static void main(String[] args) {
-    System.out.println("Minha Primeira Classe.");
+    final String BR = "Brasil";
+    System.out.println("Minha Primeira Classe no " + BR);
+
+    final double PI = 3.14;
+    final int ESTADOS_BRASILEIROS = 27;
+    final int ANO_2024 = 2024;
+
+    System.out.println("Valor pi é: " + PI);
+    System.out.println("Quantos Estados tem o Brasil? R.: " + ESTADOS_BRASILEIROS);
+    System.out.println("Em que ano estamos? R.: " + ANO_2024);
   }
 }
