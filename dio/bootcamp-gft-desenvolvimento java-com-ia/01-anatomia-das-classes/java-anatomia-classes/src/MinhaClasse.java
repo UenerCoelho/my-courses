e6f12f26e77b79ac38b1e0f717cff20e3243202e
Uener Coelho <uenercoelho2@hotmail.com>
2024-08-22 22:30:15 -0400

    DIO Bootcamp GFT - Desenvolvimento Java com AI

--- dio/bootcamp-gft-desenvolvimento java-com-ia/01-anatomia-das-classes/java-anatomia-classes/src/MinhaClasse.java
+++ dio/bootcamp-gft-desenvolvimento java-com-ia/01-anatomia-das-classes/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,7 @@
 public class MinhaClasse {
   public static void main(String[] args) {
+
+    // Declarando Variáveis
     final String BR = "Brasil";
     System.out.println("Minha Primeira Classe no " + BR);
 
@@ -8,7 +10,34 @@ public class MinhaClasse {
     final int ANO_2024 = 2024;
 
     System.out.println("Valor pi é: " + PI);
-    System.out.println("Quantos Estados tem o Brasil? R.: " + ESTADOS_BRASILEIROS);
+    System.out.println("Quantos Estados tem o Brasil? R.: " +
+        ESTADOS_BRASILEIROS);
     System.out.println("Em que ano estamos? R.: " + ANO_2024);
+
+    String meuNome = "Uener";
+    int anoFabricado = 1984;
+    boolean validação = true;
+    // String resposta;
+    // if (verdadeira = true) {
+    // resposta = verdadeiro;
+    // } else {
+    // validação = falso;
+    // }
+
+    System.out.println("Meu nome é " + meuNome + " Nascido no ano de " +
+        anoFabricado + " Isto é " + validação);
+
+    String primeiroNome = "Uener";
+    String segundoNome = "Coelho";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+
   }
+
+  // Declarando Metodos
+  public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
+  };
 }
