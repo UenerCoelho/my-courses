44952061996b651750ad393e6a8237debd11d0b2
Uener Coelho <uenercoelho2@hotmail.com>
2024-08-30 20:58:11 -0400

    DIO Bootcamp GFT - Desenvolvimento Java com AI

---
+++ dio/bootcamp-gft-desenvolvimento java-com-ia/02-tipos-e-variaveis/tipos-e-variaveis/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Tipos Primitivos, int, byte, short, long, float, double, boolean e char!");
+        int tipoInt = 21070333;
+        byte tipoByte = 123;
+        short tipoShort = 2024;
+        long tipoLong = 12345678901L;
+        float tipoFloat = 3.14F;
+        double tipoDouble = 1.99;
+        boolean tipoBoolean = true || false;
+        char tipoChar = "c";
+
+    }
+}
