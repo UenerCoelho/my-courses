7a7c28ec8080ff854aafc050c97278e77753fc80
Uener Coelho <uenercoelho2@hotmail.com>
2024-08-30 21:03:59 -0400

    DIO Bootcamp GFT - Desenvolvimento Java com AI

--- dio/bootcamp-gft-desenvolvimento java-com-ia/02-tipos-e-variaveis/tipos-e-variaveis/src/App.java
+++ dio/bootcamp-gft-desenvolvimento java-com-ia/02-tipos-e-variaveis/tipos-e-variaveis/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Tipos Primitivos, int, byte, short, long, float, double, boolean e char!");
+        // Variáveis
         int tipoInt = 21070333;
         byte tipoByte = 123;
         short tipoShort = 2024;
@@ -9,6 +10,18 @@ public class App {
         double tipoDouble = 1.99;
         boolean tipoBoolean = true || false;
         char tipoChar = "c";
+        String name = "Uener";
+
+        // Constantes, convenção de nome MAIÚSCULO
+        final int TIPO_INT = 21070333;
+        final byte TIPO_BYTE = 123;
+        final short TIPO_SHORT = 2024;
+        final long TIPO_LONG = 12345678901L;
+        final float TIPO_FLOAT = 3.14F;
+        final double TIPO_DOUBLE = 1.99;
+        final boolean TIPO_BOOLEAN = true || false;
+        final char TIPO_CHAR = "c";
+        final String NAME = "Uener";
 
     }
 }
