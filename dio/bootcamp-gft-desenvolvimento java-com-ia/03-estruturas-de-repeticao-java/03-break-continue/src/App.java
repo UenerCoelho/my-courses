6fddeb7cda8e22fd7b36ccc7e1075f3e651a318b
Uener Coelho <uenercoelho2@hotmail.com>
2024-09-17 22:41:06 -0400

    DIO Bootcamp GFT - Desenvolvimento Java com AI

--- dio/bootcamp-gft-desenvolvimento java-com-ia/03-estruturas-de-repeticao-java/03-break-continue/src/App.java
+++ dio/bootcamp-gft-desenvolvimento java-com-ia/03-estruturas-de-repeticao-java/03-break-continue/src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         for (int numero = 1; numero <= 5; numero++) {
             if (numero == 3)
                 // break;
