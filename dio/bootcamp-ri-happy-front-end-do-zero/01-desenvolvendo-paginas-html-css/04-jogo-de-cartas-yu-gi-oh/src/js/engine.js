34e4af4591dd576fceabc5b6ebad90905d64341d
Uener Coelho <uenercoelho2@hotmail.com>
2024-10-08 19:56:44 -0400

    DIO Bootcamp Ri-Happu - Front End o Zero

---
+++ dio/bootcamp-ri-happy-front-end-do-zero/01-desenvolvendo-paginas-html-css/04-jogo-de-cartas-yu-gi-oh/src/js/engine.js
@@ -0,0 +1,36 @@
+const state = {
+  score: {
+    playerScore: 0,
+    computerScore: 0,
+    scoreBox: document.getElementById('score_points')
+  },
+  cardSprites: {
+    avatar: document.getElementById('card-image'),
+    name: document.getElementById('card-name'),
+    type: document.getElementById('card-type')
+  },
+  fieldCards: {
+    player: document.getElementById('player-field-card'),
+    computer: document.getElementById('computer-field-card')
+  },
+  actions: {
+    button: document.getElementById('next-duel')
+  }
+}
+
+// const player = {
+//   player1:
+// }
+
+const cardData = [
+  {
+    id: 0,
+    name: 'Blue Eyes White Dragon',
+    type: 'Paper',
+    img: './src/assets/icons/dragon.png'
+  }
+]
+
+function init() {}
+
+init()
