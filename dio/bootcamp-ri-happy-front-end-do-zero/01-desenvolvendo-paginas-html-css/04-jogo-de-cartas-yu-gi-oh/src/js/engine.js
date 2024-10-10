5aac5d3e2c888741227a495fc96fd33135cd99f4
Uener Coelho <uenercoelho2@hotmail.com>
2024-10-10 19:09:39 -0400

    DIO Bootcamp Ri-Happu - Front End o Zero

--- dio/bootcamp-ri-happy-front-end-do-zero/01-desenvolvendo-paginas-html-css/04-jogo-de-cartas-yu-gi-oh/src/js/engine.js
+++ dio/bootcamp-ri-happy-front-end-do-zero/01-desenvolvendo-paginas-html-css/04-jogo-de-cartas-yu-gi-oh/src/js/engine.js
@@ -18,9 +18,10 @@ const state = {
   }
 }
 
-// const player = {
-//   player1:
-// }
+const playerSides = {
+  player1: 'player-fiel-card',
+  computer: 'computer-field-card'
+}
 const pathImages = './src/assets/icons'
 const cardData = [
   {
@@ -49,6 +50,43 @@ const cardData = [
   }
 ]
 
-function init() {}
+async function getRandomCardId() {
+  const randomIndex = Math.floor(Math.random() * cardData.length)
+  return cardData[randomIndex].id
+}
+
+async function createCardImage(IdCard, fieldSide) {
+  const cardImage = document.createElement('img')
+  cardImage.setAttribute('height', '100px')
+  cardImage.setAttribute('src', './src/assets/icons/card-back.png')
+  cardImage.setAttribute('data-id', IdCard)
+  cardImage.classList.add('card')
+
+  if (fieldSide === playerSides.player1) {
+    cardImage.addEventListener('click', () => {
+      setCardsField(cardImage.getAttribute('data-id'))
+    })
+  }
+
+  cardImage.addEventListener('mouseover', () => {
+    drawSelectedCard(IdCard)
+  })
+
+  return cardImage
+}
+
+async function drawCards(cardNumbers, fieldSide) {
+  for (let i = 0; I < cardNumbers; i++) {
+    const randomIdCard = await getRandomCardId()
+    const cardImage = await createCardImage(randomIdCard, fieldSide)
+
+    document.getElementById(fieldSide).appendChild(cardImage)
+  }
+}
+
+function init() {
+  drawCards(5, playerSides.player1)
+  drawCards(5, playerSides.computer)
+}
 
 init()
