8eabf82f1b7c4bfb956c4d26662df9bb2af3dd64
Uener Coelho <uenercoelho2@hotmail.com>
2024-10-11 21:12:10 -0400

    DIO Bootcamp Ri-Happu - Front End o Zero

--- dio/bootcamp-ri-happy-front-end-do-zero/01-desenvolvendo-paginas-html-css/04-jogo-de-cartas-yu-gi-oh/src/js/engine.js
+++ dio/bootcamp-ri-happy-front-end-do-zero/01-desenvolvendo-paginas-html-css/04-jogo-de-cartas-yu-gi-oh/src/js/engine.js
@@ -75,6 +75,33 @@ async function createCardImage(IdCard, fieldSide) {
   return cardImage
 }
 
+async function setCardsField(cardId) {
+  await removeAllCardsImages()
+
+  let computerCardId = await getRandomCardId()
+
+  state.fieldCards.player.style.display = 'block'
+  state.fieldCards.computer.style.display = 'block'
+
+  state.fieldCards.player.src = cardData[cardId].img
+  state.fieldCards.computer.src = cardData[computerCardId].img
+
+  let duelResults = await checkDuelResults(cardId, computerCardId)
+
+  await updateScore()
+  await drawButton(duelResults)
+}
+
+async function removeAllCardsImages() {
+  let cards = document.querySelector('#computer-cards')
+  let imgElements = cards.querySelectorAll('img')
+  imgElements.forEach(img => img.remove())
+
+  cards = document.querySelector('#player-cards')
+  imgElements = cards.querySelectorAll('img')
+  imgElements.forEach(img => img.remove())
+}
+
 async function drawSelectCard(index) {
   state.cardSprites.avatar.src = cardData[index].img
   state.cardSprites.name.innerText = cardData[index].name
