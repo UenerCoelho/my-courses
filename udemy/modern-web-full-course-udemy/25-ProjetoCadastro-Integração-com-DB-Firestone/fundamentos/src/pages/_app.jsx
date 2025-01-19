4f200787cf767f457b33f3056e2315492939b1c9
Uener Coelho <uenercoelho2@hotmail.com>
2025-01-18 22:36:14 -0400

    Udemy - Modern Course Full Stack

--- udemy/modern-web-full-course-udemy/25-ProjetoCadastro-Integração-com-DB-Firestone/fundamentos/src/pages/_app.jsx
+++
@@ -1,9 +0,0 @@
-import '@/src/styles/globals.css'
-import '@/src/styles/app.css'
-import '@/src/styles/Estiloso.modules.css'
-
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default App
