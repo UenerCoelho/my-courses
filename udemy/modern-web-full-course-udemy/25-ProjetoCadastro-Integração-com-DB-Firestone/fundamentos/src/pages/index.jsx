9ede8c9b6644611ac9d043e69efef012b15e9f75
Uener Coelho <uenercoelho2@hotmail.com>
2025-01-22 20:13:24 -0400

    Udemy - Curso Web Moderno Completo

--- udemy/modern-web-full-course-udemy/25-ProjetoCadastro-Integração-com-DB-Firestone/fundamentos/src/pages/index.jsx
+++ udemy/modern-web-full-course-udemy/25-ProjetoCadastro-Integração-com-DB-Firestone/fundamentos/src/pages/index.jsx
@@ -1,8 +1,9 @@
-export default function App(){
-  return (
-    <div>
-      <h1>Teste</h1>
-      <h4>Testículo</h4>
-    </div>
+export default function Inicio(){
+  return ( 
+      <div>
+        <h1>Início</h1>
+        <h2>Teste médio</h2>
+        <h3>Testículos</h3>
+      </div>
   )
 }
