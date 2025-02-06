5b92f3766c9d8caf260369cc6f5565678f7c3d42
Uener Coelho <uenercoelho2@hotmail.com>
2025-02-05 21:50:36 -0400

    Udemy - Curso Web Moderno Completo

--- udemy/modern-web-full-course-udemy/25-ProjetoCadastro-Integração-com-DB-Firestone/fundamentos/src/pages/index.jsx
+++ udemy/modern-web-full-course-udemy/25-ProjetoCadastro-Integração-com-DB-Firestone/fundamentos/src/pages/index.jsx
@@ -14,9 +14,9 @@ export default function Inicio(){
         <Navegador destino='/estiloso' texto='Estiloso'/>
         <Navegador destino='/exemplo' texto='Exemplo' cor='darkviolet'/>
         <Navegador destino='/navegacao' texto='Navegação #01' cor='darkgreen'/>
-        <Navegador destino='/cliente/123' texto='Navegação #02' cor='darkgray'/>
+        <Navegador destino='/cliente/123' texto='Navegação #02' cor='#42a8a8'/>
         <Navegador destino='/estado' texto='Componente com Estado' cor='darkblue'/>
-        <Navegador destino='/integracao' texto='Integração com API' cor='purple'/>
+        <Navegador destino='/integracao' texto='Integração com API' cor='#a45b71'/>
 
       </div>
   )
