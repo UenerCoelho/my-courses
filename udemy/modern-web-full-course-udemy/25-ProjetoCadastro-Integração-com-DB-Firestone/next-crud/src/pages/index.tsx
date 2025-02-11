88cc1fb85168f8ae3983dd80145bc8cedec4b1e4
Uener Coelho <uenercoelho2@hotmail.com>
2025-02-10 23:31:30 -0400

    Udemy - Curso Web Moderno Completo

--- udemy/modern-web-full-course-udemy/25-ProjetoCadastro-Integração-com-DB-Firestone/next-crud/src/pages/index.tsx
+++ udemy/modern-web-full-course-udemy/25-ProjetoCadastro-Integração-com-DB-Firestone/next-crud/src/pages/index.tsx
@@ -1,12 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div className={``}>
-      <span className='text-4xl'>Texto</span>
+    <div className={`
+      flex justify-center items-center h-screen 
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+      `}>
+        <Layout titulo="Cadastro Simples">
+          <span>Conteúdo</span>
+        </Layout>
     </div>
   )
 }
