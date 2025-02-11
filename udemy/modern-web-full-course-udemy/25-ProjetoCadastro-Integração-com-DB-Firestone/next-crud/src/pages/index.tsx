05354c35f78ea8cd9dce622acbba4eb80f874fdf
Uener Coelho <uenercoelho2@hotmail.com>
2025-02-10 22:41:11 -0400

    Udemy - Curso Web Moderno Completo

---
+++ udemy/modern-web-full-course-udemy/25-ProjetoCadastro-Integração-com-DB-Firestone/next-crud/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+
+export default function Home() {
+  return (
+    <div className={``}>
+      <span className='text-4xl'>Texto</span>
+    </div>
+  )
+}
