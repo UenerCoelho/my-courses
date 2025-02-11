cddb4750c9cabeb3e337c268d755afd740e88b74
Uener Coelho <uenercoelho2@hotmail.com>
2025-02-11 11:45:07 -0400

    Udemy - Curso Web Moderno Completo

--- udemy/modern-web-full-course-udemy/25-ProjetoCadastro-Integração-com-DB-Firestone/next-crud/src/components/Tabela.tsx
+++ udemy/modern-web-full-course-udemy/25-ProjetoCadastro-Integração-com-DB-Firestone/next-crud/src/components/Tabela.tsx
@@ -19,7 +19,11 @@ export default function Tabela(props: TabelaProps) {
   function renderizarDados() {
     return props.clientes?.map((cliente, i) => {
       return (
-        <tr key={cliente.id} className={`${i % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}`}>
+        <tr key={cliente.id} className={`
+          ${ i % 2 === 0 
+          ? 'bg-purple-400' 
+          : 'bg-purple-300'}
+          `}>
           <td className="text-left p-4">{cliente.id}</td>
           <td className="text-left p-4">{cliente.nome}</td>
           <td className="text-left p-4">{cliente.idade}</td>
@@ -29,8 +33,11 @@ export default function Tabela(props: TabelaProps) {
   }
 
   return (
-    <table>
-      <thead>
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead className={`
+      text-gray-100
+        bg-gradient-to-r from-purple-500 to-purple-800
+        `}>
       {renderizarCabecalho()}
       </thead>
       <tbody>
