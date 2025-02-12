0e5e6c28550187376799dd24cb317f1a2191f0ad
Uener Coelho <uenercoelho2@hotmail.com>
2025-02-11 21:17:54 -0400

    Udemy - Curso Web Moderno Completo

--- udemy/modern-web-full-course-udemy/25-ProjetoCadastro-Integração-com-DB-Firestone/next-crud/src/pages/index.tsx
+++ udemy/modern-web-full-course-udemy/25-ProjetoCadastro-Integração-com-DB-Firestone/next-crud/src/pages/index.tsx
@@ -6,6 +6,9 @@ import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
 
 export default function Home() {
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+
 
   const clientes = [
     new Cliente('Ana', 34, '1'),
@@ -15,18 +18,23 @@ export default function Home() {
   ]
 
   function clienteSelecionado(cliente: Cliente) {
-    console.log(cliente.nome)
+    setCliente(cliente)
+    setVisivel('form')
   }
 
   function clienteExcluido(cliente: Cliente) {
     console.log(`Excluir... ${cliente.nome}`)
   }
 
-  function salvarCliente(cliente: Cliente) {
-    console.log(cliente)
+  function novoCliente() {
+    setCliente(Cliente.vazio())
+    setVisivel('form')
   }
 
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente)
+    setVisivel('tabela')
+  }
 
   return (
     <div className={`
@@ -39,7 +47,7 @@ export default function Home() {
             <>
               <div className="flex justify-end">
                 <Botao cor="green" className="mb-4"
-                  onClick={() => setVisivel('form')}>
+                  onClick={novoCliente}>
                   Novo Cliente
                 </Botao>
               </div>
@@ -49,13 +57,12 @@ export default function Home() {
               />
             </>
           ) : (
-            <Formulario />
-          )}
-          <Formulario 
-            cliente={clientes[0]}
+            <Formulario 
+            cliente={cliente}
             clienteMudou={salvarCliente}
             cancelado={() => setVisivel('tabela')}
             />
+          )}
         </Layout>
     </div>
   )
